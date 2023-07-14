00907e71a20c00724ac899d3408647b1ae1891b0
Deyvi <deyvi.crp@gmail.com>
2023-07-14 15:08:42 -0500

    roles update

--- src/controllers/rolController.js
+++ src/controllers/rolController.js
@@ -15,36 +15,36 @@ export const listarRol = async(req, res) =>{
         // if(getreply) 
         //     return res.status(200).json({mensaje:"lista de roles", data: JSON.parse(getreply)})
 
-        clientRedis.get('clave', async(error, respuesta) => {
-            if (respuesta) 
-                return res.status(200).json({mensaje:"lista de roles", data: JSON.parse(respuesta)})
-            console.log("aaaa")
-            //consulta a la BD
-            const [rows] = await pool.query('CALL sp_listar_rol');
+        // clientRedis.get('clave', async(error, respuesta) => {
+        //     if (respuesta) 
+        //         return res.status(200).json({mensaje:"lista de roles", data: JSON.parse(respuesta)})
+        //     console.log("aaaa")
+        //     //consulta a la BD
+        //     const [rows] = await pool.query('CALL sp_listar_rol');
 
-            if(rows[0].length <= 0){
-                return res.status(404).json({mensaje: 'Lista de rol vacía',data: []});
-            }
+        //     if(rows[0].length <= 0){
+        //         return res.status(404).json({mensaje: 'Lista de rol vacía',data: []});
+        //     }
             
-            // await SET_REDIS_ASYNC("roles", JSON.stringify(rows[0]))
+        //     // await SET_REDIS_ASYNC("roles", JSON.stringify(rows[0]))
 
-            clientRedis.set('clave',JSON.stringify(rows[0]) ,async(error, respuesta) => {
-            if (error) throw error;
-                return res.status(200).json({mensaje: 'lista de roles',data: rows[0]});
-            });
-        });
+        //     clientRedis.set('clave',JSON.stringify(rows[0]) ,async(error, respuesta) => {
+        //     if (error) throw error;
+        //         return res.status(200).json({mensaje: 'lista de roles',data: rows[0]});
+        //     });
+        // });
 
-        // console.log("aaaa")
-        // //consulta a la BD
-        // const [rows] = await pool.query('CALL sp_listar_rol');
+        console.log("aaaa")
+        //consulta a la BD
+        const [rows] = await pool.query('CALL sp_listar_rol');
 
-        // if(rows[0].length <= 0){
-        //     return res.status(404).json({mensaje: 'Lista de rol vacía',data: []});
-        // }
+        if(rows[0].length <= 0){
+            return res.status(404).json({mensaje: 'Lista de rol vacía',data: []});
+        }
         
         // await SET_REDIS_ASYNC("roles", JSON.stringify(rows[0]))
 
-        // return res.status(200).json({mensaje: 'lista de roles',data: rows[0]});
+        return res.status(200).json({mensaje: 'lista de roles',data: rows[0]});
     }catch(error){
         console.log(error)
         return res.status(500).json({mensaje:'Error al listar los roles',error: error});
